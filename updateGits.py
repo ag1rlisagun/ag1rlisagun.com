09961f2ae258ac25f06c6867b81278d8cf2729f6
ag1rlisagun <aaliyahwusu@gmail.com>
2025-06-10 03:02:59 -0600

    script

--- updateGits.py
+++ updateGits.py
@@ -28,7 +28,7 @@ os.system("git push")
 
 # update nekogit repo folder
 os.chdir("/Users/aaliyahwusu/Documents/projects/ag1rlisasite")
-subprocess.call(["rsync", "-vaPR", "ag1rlisagun.com/", "/Users/aaliyahwusu/Documents/projects/ag1rlisasite/nekogit/ag1rlisagun"])
+subprocess.call(["rsync", "-varP", "ag1rlisagun.com/", "/Users/aaliyahwusu/Documents/projects/ag1rlisasite/nekogit/ag1rlisagun"])
 
 # push changes to nekogit repo
 os.chdir("/Users/aaliyahwusu/Documents/projects/ag1rlisasite/nekogit/ag1rlisagun")
