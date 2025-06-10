829f3f29dc53edece1a35f2e497e553460fe52b1
ag1rlisagun <aaliyahwusu@gmail.com>
2025-06-10 14:48:20 -0600

    script

--- updateGits.py
+++ updateGits.py
@@ -20,25 +20,24 @@ for word in commandArg:
         message += word + "\"" # end the message
 
 # commit and push changes to main github
-os.system("git remote set-url origin https://github.com/ag1rlisagun.com.git")
+os.system("git remote -v")
+os.system("git status")
 os.system("git pull")
-os.system("git status")
 os.system("git add .")
-os.system("git status")
 os.system("git commit -m {0}".format(message))
 os.system("git push")
 
 # update nekogit repo folder
 os.chdir("/Users/aaliyahwusu/Documents/projects/ag1rlisasite")
-subprocess.call(["rsync", "-varP", "ag1rlisagun.com/", "/Users/aaliyahwusu/Documents/projects/ag1rlisasite/nekogit/ag1rlisagun"])
+subprocess.call(["rsync", "-varP", "ag1rlisagun.com/", "/Users/aaliyahwusu/Documents/projects/ag1rlisasite/ag1rlisagun.nekoweb.org"])
 
 # push changes to nekogit repo
-os.chdir("/Users/aaliyahwusu/Documents/projects/ag1rlisasite/nekogit/ag1rlisagun")
+os.chdir("/Users/aaliyahwusu/Documents/projects/ag1rlisasite/ag1rlisagun.nekoweb.org")
 os.system("git remote set-url origin https://git.nekoweb.org/ag1rlisagun.git")
+os.system("git remote -v")
+os.system("git status")
 os.system("git pull")
-os.system("git status")
 os.system("git add .")
-os.system("git status")
 os.system("git commit -m {0}".format(message))
 os.system("git push -u origin main")
 os.chdir("/Users/aaliyahwusu/Documents/projects/ag1rlisasite")
